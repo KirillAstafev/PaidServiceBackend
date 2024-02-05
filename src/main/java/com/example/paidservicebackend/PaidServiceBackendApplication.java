833fb78dc6b234101e5a1670b58f56d8c2d7923c
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2024-02-05 20:55:07 +0300

    Стартовый проект

---
+++ src/main/java/com/example/paidservicebackend/PaidServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.paidservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaidServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaidServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paidservicebackend/PaidServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paidservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaidServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
